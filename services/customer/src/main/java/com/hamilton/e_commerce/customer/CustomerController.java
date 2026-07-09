b057c2568086f7621633493f69ced9bbc3c65e83
hamilton2959 <skytech2959@gmail.com>
2026-07-09 11:59:00 +0300

    init commit

--- services/customer/src/main/java/com/hamilton/e_commerce/customer/CustomerController.java
+++ services/customer/src/main/java/com/hamilton/e_commerce/customer/CustomerController.java
@@ -15,16 +15,12 @@ public class CustomerController {
   private final CustomerService service;
 
   @PostMapping
-  public ResponseEntity<String> createCustomer(
-      @RequestBody @Valid CustomerRequest request
-  ) {
+  public ResponseEntity<String> createCustomer(@RequestBody @Valid CustomerRequest request) {
     return ResponseEntity.ok(this.service.createCustomer(request));
   }
 
   @PutMapping
-  public ResponseEntity<Void> updateCustomer(
-      @RequestBody @Valid CustomerRequest request
-  ) {
+  public ResponseEntity<Void> updateCustomer(@RequestBody @Valid CustomerRequest request) {
     this.service.updateCustomer(request);
     return ResponseEntity.accepted().build();
   }
@@ -35,23 +31,17 @@ public class CustomerController {
   }
 
   @GetMapping("/exists/{customer-id}")
-  public ResponseEntity<Boolean> existsById(
-      @PathVariable("customer-id") String customerId
-  ) {
+  public ResponseEntity<Boolean> existsById(@PathVariable("customer-id") String customerId) {
     return ResponseEntity.ok(this.service.existsById(customerId));
   }
 
   @GetMapping("/{customer-id}")
-  public ResponseEntity<CustomerResponse> findById(
-      @PathVariable("customer-id") String customerId
-  ) {
+  public ResponseEntity<CustomerResponse> findById(@PathVariable("customer-id") String customerId) {
     return ResponseEntity.ok(this.service.findById(customerId));
   }
 
   @DeleteMapping("/{customer-id}")
-  public ResponseEntity<Void> delete(
-      @PathVariable("customer-id") String customerId
-  ) {
+  public ResponseEntity<Void> delete(@PathVariable("customer-id") String customerId) {
     this.service.deleteCustomer(customerId);
     return ResponseEntity.accepted().build();
   }
